ee87d8c689ec91e7ec1556d84709943fe3c610c9
Roshansth <104548526+Roshansth@users.noreply.github.com>
2022-08-28 22:15:48 +1000

    Add files via upload

---
+++ Js/questions.js
@@ -0,0 +1,134 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What are two types of network layer firewalls?",
+    answer: "Dynamic and static",
+    options: [
+      "Stateful",
+      "stateless",
+      "Dynamic and static",
+      "Stateful and stateless"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which of the following attacks requires a carrier file to self-replicate?",
+    answer: "Trojan",
+    options: [
+      "Trojan",
+      "Virus",
+      "Worm",
+      "Cascading"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Web page editors works on a ____ principle",
+    answer: "WYSIWYG",
+    options: [
+      "WYSIWYG",
+      "Hypertext Programming",
+      "WWW",
+      " WYGWYSI"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which program is used by web clients to view the web pages?",
+    answer: "Web browser",
+    options: [
+      "Protocol",
+      "Web server",
+      "Search Engine",
+      "Web browser"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What is the name of the location address of the hypertext documents?",
+    answer: "Web server",
+    options: [
+      "Web address",
+      "File",
+      "Uniform Resource Locator",
+      "Web server"
+    ]
+  },
+  {
+    numb: 6,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+	
+  },
+  
+  {
+    numb: 7,
+    question: "What are shared on the Internet and are called as Web pages?",
+    answer: "Hypertext documents",
+    options: [
+      " None",
+      "Hypertext documents",
+      "Programs",
+      "Cables"
+    ]
+  },
+  
+  {
+    numb: 8,
+    question: "How many colour names are used by the browsers?",
+    answer: "16",
+    options: [
+      "10",
+      "16",
+      "15",
+      "20"
+    ]
+  },
+  {
+    numb: 9,
+    question: "Which tag is used to display text in title bar of a web document?",
+    answer: "Title tag",
+    options: [
+      "Comment Tag",
+      "Body tag",
+      "Meta tag",
+      "Title tag"
+    ]
+  },
+  
+  {
+    numb: 10,
+    question: "The ____ attribute is used to identify the values of variables.",
+    answer: "content",
+    options: [
+      "http-equiv",
+      "text",
+      "name",
+      "content"
+    ]
+  },
+
+  
+
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
